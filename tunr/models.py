eed49336dfe97a8079eca130b0b86bb3a1e928f1
Dalton Hart <DaltonVaren@gmail.com>
2019-08-05 17:23:44 -0700

    adds urls and views for artists and songs

--- tunr/models.py
+++ tunr/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+  name = models.CharField(max_length=100)
+  nationality = models.CharField(max_length=100)
+  photo_url = models.TextField()
+  spotify_link = models.TextField(default='https://www.spotify.com/')
+
+  def __str__(self):
+    return self.name
+  
+  def hello(self):
+    return f"I am {self.name}"
+
+class Song(models.Model):
+  spotify_preview = models.TextField(default="https://www.spotify.com/")
+  title = models.CharField(max_length=100, default="untitled")
+  genre = models.CharField(max_length=100, default="unassigned")
+  length = models.PositiveIntegerField(default="2")
+  artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
+
+  def __str__(self):
+    return f"{self.title}-{self.artist}"
--- tunr/views.py
+++ tunr/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
+from .models import Artist, Song
 # Create your views here.
+
+def home(request):
+  return HttpResponse("You are home")
+
+def json_res(request):
+  return JsonResponse({ "status" : "Ok" })
+
+def artist_list(request):
+  artists = Artist.objects.all()
+  return render(request, 'artist_list.html', {"artists": artists})
+
+def song_list(request):
+  songs = Song.objects.all()
+  return render(request, 'song_list.html', {"songs": songs})
+
+def artist_detail(request, pk):
+  artist = Artist.objects.get(id=pk)
+  return render(request, 'artist_detail.html', {"artist":artist})
