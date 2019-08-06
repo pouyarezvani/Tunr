a4b136fbb3fcee02eb76fd98abdac04f14322138
pouyarezvani1 <pouyarezvani10@gmail.com>
2019-08-06 13:54:41 -0700

    Adds full CRUD

--- tunr/urls.py
+++ tunr/urls.py
@@ -4,6 +4,12 @@ from . import views
 urlpatterns = [
   path('', views.artist_list, name='artist_list'),
   path('artists/<int:pk>', views.artist_detail, name='artist_detail'),
+  path('artist/<int:pk>/edit', views.artist_edit, name='artist_edit'),
+  path('artists/<int:pk>/delete', views.artist_delete, name='artist_delete'),
+  path('artists/new', views.artist_create, name='artist_create'),
+  path('artists/<int:pk>/songs/new', views.song_create, name='song_create'),
   path('songs', views.song_list, name='song_list'),
-  path('status', views.json_res, name='status' )
+  path('songs/<int:pk>/edit', views.song_edit, name='song_edit'),
+  path('songs/<int:pk>/delete', views.song_delete, name='song_delete'),
+  path('status', views.json_res, name='status')
 ]
--- tunr/views.py
+++ tunr/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from .models import Artist, Song
+from .forms import ArtistForm, SongForm
 # Create your views here.
 
 def home(request):
@@ -20,3 +21,57 @@ def song_list(request):
 def artist_detail(request, pk):
   artist = Artist.objects.get(id=pk)
   return render(request, 'artist_detail.html', {"artist":artist})
+
+def artist_create(request):
+  if request.method == 'POST':
+    form = ArtistForm(request.POST)
+    if form.is_valid():
+      artist = form.save()
+      return redirect('artist_detail', pk=artist.pk)
+  else:
+    form = ArtistForm()
+  return render(request, 'artist_form.html', {'form': form, 'header': 'New Artist'})
+
+def artist_edit(request, pk):
+  artist = Artist.objects.get(id=pk)
+  if request.method == 'POST':
+    form = ArtistForm(request.POST, instance=artist)
+    if form.is_valid():
+      artist = form.save()
+      return redirect('artist_detail', pk=artist.pk)
+  else:
+    form = ArtistForm(instance=artist)
+  return render(request, 'artist_form.html', {'form': form, 'header':f'Edit{artist.name}'})
+
+def artist_delete(request, pk):
+  Artist.objects.get(id=pk).delete()
+  return redirect('artist_list')
+
+def song_create(request, pk):
+  artist = Artist.objects.get(id=pk)
+  if request.method == 'POST':
+    form = SongForm(request.POST)
+    if form.is_valid():
+      song = form.save(commit=False)
+      song.artist = artist
+      song.save()
+      return redirect('artist_detail', pk=song.artist.pk)
+  else:
+    form = SongForm()
+  return render(request, 'artist_form.html', {'form': form, 'header': f'Add Song for {artist.name}'})
+
+def song_edit(request, pk):
+  song = Song.objects.get(id=pk)
+  if request.method == 'POST':
+    form = SongForm(request.POST, instance=song)
+    if form.is_valid():
+      song = form.save()
+      return redirect('artist_detail', pk=song.artist.pk)
+  else:
+    form = SongForm()
+  return render(request, 'artist_form.html', {'form': form, 'header':f'Edit{song.title}'})
+
+
+def song_delete(request, pk):
+  Song.objects.get(id=pk).delete()
+  return redirect('song_list')
